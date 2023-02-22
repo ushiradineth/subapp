c8bcd54ed7acd0485ffa06cfc279f423716422ac
Ushira Dineth <ushiradineth@gmail.com>
2023-02-22 21:30:38 +0530

    added styling

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { styled } from "nativewind";
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <StyledView className="flex-1 items-center justify-center bg-red-300">
+      <StyledText className="font-bold">Hello world.</StyledText>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
