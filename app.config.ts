fc5dfcd8def916ac7034ca1b1615f1ff457bce33
Ushira Dineth <ushiradineth@gmail.com>
2023-02-22 21:30:30 +0530

    updated config

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default {
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
-    userInterfaceStyle: "light",
+    userInterfaceStyle: "darl",
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
--- babel.config.ts
+++ babel.config.ts
@@ -1,4 +1,4 @@
-module.exports = function (api) {
+module.exports = function(api: { cache: (arg0: boolean) => void; }) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
