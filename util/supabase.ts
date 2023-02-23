142ac6a82569d9abd8eb8901454df6d7ddf33fd1
Ushira Dineth <ushiradineth@gmail.com>
2023-02-23 13:33:00 +0530

    added supabase

---
+++ util/supabase.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
