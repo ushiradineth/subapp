cab114cc1efed99b0070091b36df8c5203ab4aed
Ushira Dineth <ushiradineth@gmail.com>
2023-02-28 20:58:28 +0530

    added auth checkpoint

--- App.tsx
+++ App.tsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/components/HomeScreen";
 import ProfileScreen from "./src/components/ProfileScreen";
+import AuthScreen from "./src/components/AuthScreen";
 import { NativeBaseProvider } from "native-base";
 
 type RootStackParamList = {
   Home: undefined;
   Profile: { name: string };
+  Authentication: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+  const isSignedIn = true;
+
   return (
     <NativeBaseProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
+          {isSignedIn ? (
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+            </>
+          ) : (
+            <Stack.Screen name="Authentication" component={AuthScreen} />
+          )}
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
