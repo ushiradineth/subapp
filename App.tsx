2f2051f82881a60b5da2f2ef462dce1770b79af7
Ushira Dineth <ushiradineth@gmail.com>
2023-03-06 15:15:03 +0530

    reconfigured routing

--- App.tsx
+++ App.tsx
@@ -1,37 +1,20 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./src/components/HomeScreen";
-import ProfileScreen from "./src/components/ProfileScreen";
-import AuthScreen from "./src/components/AuthScreen";
+import { AuthProvider } from "./src/utils/AuthProvider";
 import { NativeBaseProvider } from "native-base";
-
-type RootStackParamList = {
-  Home: undefined;
-  Profile: { name: string };
-  Authentication: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import Navigation from "./src/navigation";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "react-native-rapi-ui";
 
 function App() {
-  const isSignedIn = true;
-
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {isSignedIn ? (
-            <>
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Profile" component={ProfileScreen} />
-            </>
-          ) : (
-            <Stack.Screen name="Authentication" component={AuthScreen} />
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <ThemeProvider>
+      <NativeBaseProvider>
+        <AuthProvider>
+          <Navigation />
+        </AuthProvider>
+        <StatusBar />
+      </NativeBaseProvider>
+    </ThemeProvider>
   );
 }
 
