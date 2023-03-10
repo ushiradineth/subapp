24ec6758125e7359a5e0799fc09fd5c90b22e817
Ushira Dineth <ushiradineth@gmail.com>
2023-03-10 15:48:04 +0530

    fixed config and added env

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ["babel-preset-expo"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ],
+};
--- babel.config.ts
+++
@@ -1,6 +0,0 @@
-module.exports = function(api: { cache: (arg0: boolean) => void; }) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-  };
-};
