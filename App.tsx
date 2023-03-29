0105a436d244ae8965e44a4afce83af3119016ff
Ushira Dineth <ushiradineth@gmail.com>
2023-03-29 22:03:51 +0530

    removed native base

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { AuthProvider } from "./src/utils/AuthProvider";
-import { NativeBaseProvider } from "native-base";
 import Navigation from "./src/navigation";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "react-native-rapi-ui";
@@ -8,12 +7,10 @@ import { ThemeProvider } from "react-native-rapi-ui";
 function App() {
   return (
     <ThemeProvider theme="dark">
-      <NativeBaseProvider>
         <AuthProvider>
           <Navigation />
         </AuthProvider>
         <StatusBar />
-      </NativeBaseProvider>
     </ThemeProvider>
   );
 }
