c38797cdf4111fe5c510d3837f89d003de358a4d
Ushira Dineth <ushiradineth@gmail.com>
2023-03-30 21:01:57 +0530

    added tamagui

--- App.tsx
+++ App.tsx
@@ -2,20 +2,16 @@ import React from "react";
 import Navigation from "./src/navigation";
 import { AuthProvider } from "./src/utils/AuthProvider";
 import { ThemeProvider } from "./src/utils/Theme";
-import config from "./tamagui.config";
 import { StatusBar } from "expo-status-bar";
-import { TamaguiProvider } from "tamagui";
 
 function App() {
   return (
-    <TamaguiProvider config={config}>
-      <ThemeProvider>
-        <AuthProvider>
-          <Navigation />
-        </AuthProvider>
-        <StatusBar />
-      </ThemeProvider>
-    </TamaguiProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
+      <StatusBar />
+    </ThemeProvider>
   );
 }
 
--- src/utils/Theme.tsx
+++ src/utils/Theme.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useColorScheme } from "react-native";
-import { Theme } from "tamagui";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
+import config from "../../tamagui.config";
+import { TamaguiProvider, Theme } from "tamagui";
 
 const ThemeContext = React.createContext<{
   theme: "light" | "dark";
@@ -18,7 +21,11 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <Theme name={theme}>{children}</Theme>
+      <TamaguiProvider config={config}>
+        <ApplicationProvider {...eva} theme={theme === "light" ? eva.light : eva.dark}>
+          <Theme name={theme}>{children}</Theme>
+        </ApplicationProvider>
+      </TamaguiProvider>
     </ThemeContext.Provider>
   );
 };
