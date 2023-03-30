396c2777fe185484912f0bf87ffde1b188b85cb4
Ushira Dineth <ushiradineth@gmail.com>
2023-03-30 17:56:02 +0530

    added tamagui

--- app.config.ts
+++ app.config.ts
@@ -5,32 +5,30 @@ export default {
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
-    userInterfaceStyle: "darl",
+    userInterfaceStyle: "automatic",
     splash: {
       image: "./assets/splash.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
     },
     updates: {
-      "fallbackToCacheTimeout": 0
+      fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      }
+        backgroundColor: "#FFFFFF",
+      },
     },
     extra: {
       eas: {
-        projectId: "0dc6749a-cb3d-4fe0-80aa-f2a7ba811017"
+        projectId: "0dc6749a-cb3d-4fe0-80aa-f2a7ba811017",
       },
     },
     owner: "ushiradineth",
-  }
-}
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,33 @@
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: [
-    [
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-      },
+process.env.TAMAGUI_TARGET = "native";
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "@tamagui/babel-plugin",
+        {
+          components: ["tamagui"],
+          config: "./tamagui.config.ts",
+          logTimings: true,
+        },
+      ],
+      [
+        "transform-inline-environment-variables",
+        {
+          include: "TAMAGUI_TARGET",
+        },
+      ],
+      "react-native-reanimated/plugin",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
     ],
-  ],
+  };
 };
