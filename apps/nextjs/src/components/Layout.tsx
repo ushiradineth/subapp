5add66365a2e320ff6efe7eada2064e42abbebcc
Ushira Dineth <ushiradineth@gmail.com>
2023-05-20 21:53:51 +0530

    fix: fixed dark mode

--- apps/nextjs/src/components/Layout.tsx
+++ apps/nextjs/src/components/Layout.tsx
@@ -7,6 +7,7 @@ import { signOut, useSession } from "next-auth/react";
 
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from "~/components/ui/menubar";
 import icon from "../../public/logo.svg";
+import { Button } from "./ui/button";
 
 function Layout(props: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
@@ -19,30 +20,35 @@ function Layout(props: { children: React.ReactNode }) {
   if (status === "authenticated" && router.pathname === "/auth") router.push("/");
 
   return (
-    <main className="bg-bgc h-screen">
+    <main className="bg-bgc dark h-screen">
       <div className={`border-bc flex h-14 items-center border-b ${router.pathname === "/auth" && "hidden"}`}>
         <Link href={"/"}>
           <Image src={icon} alt="SubM Logo" width={120} className="ml-4" />
         </Link>
-        {/* {router.pathname === "/" && status !== } */}
-        <Menubar className="ml-auto mr-4 w-fit">
-          <MenubarMenu>
-            <MenubarTrigger>
-              <User className="w-8 text-black" />
-            </MenubarTrigger>
-            <MenubarContent>
-              <Profile />
-              <MenubarSeparator />
-              <Link href={`/profile/${session?.user.id}`}>
-                <MenubarItem>Profile</MenubarItem>
-              </Link>
-              <Link href={`/settings`}>
-                <MenubarItem>Settings</MenubarItem>
-              </Link>
-              <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
+        {status === "unauthenticated" ? (
+          <Button className="ml-auto mr-4 w-fit" onClick={() => router.push("/auth")}>
+            Authenticate
+          </Button>
+        ) : (
+          <Menubar className="dark ml-auto mr-4 w-fit">
+            <MenubarMenu>
+              <MenubarTrigger className="m-0 p-2">
+                <User className="text-white" />
+              </MenubarTrigger>
+              <MenubarContent className="dark text-white">
+                <Profile />
+                <MenubarSeparator />
+                <Link href={`/profile/${session?.user.id}`}>
+                  <MenubarItem>Profile</MenubarItem>
+                </Link>
+                <Link href={`/settings`}>
+                  <MenubarItem>Settings</MenubarItem>
+                </Link>
+                <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        )}
       </div>
       <div className={`flex flex-grow flex-col items-center justify-center text-white ${router.pathname !== "/auth" && "my-10"}`}>{props.children}</div>
     </main>
--- apps/nextjs/src/components/ui/menubar.tsx
+++ apps/nextjs/src/components/ui/menubar.tsx
@@ -38,7 +38,7 @@ const MenubarTrigger = React.forwardRef<
   <MenubarPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
+      "flex cursor-default select-none items-center text-sm font-medium outline-none",
       className
     )}
     {...props}
