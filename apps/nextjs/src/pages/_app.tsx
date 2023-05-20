b6179e9880012039ec10bbb5021554dfa5b223a7
Ushira Dineth <ushiradineth@gmail.com>
2023-05-20 16:38:48 +0530

    fix: added layout

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -4,13 +4,14 @@ import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
-import NavigationBar from "~/components/NavigationBar";
+import Layout from "~/components/Layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-        <NavigationBar />
+      <Layout>
         <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 };
