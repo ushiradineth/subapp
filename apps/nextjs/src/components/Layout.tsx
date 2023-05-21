ae4789f912a906e386b35cd736ce0fb032ca913a
Ushira Dineth <ushiradineth@gmail.com>
2023-05-21 15:58:29 +0530

    feat(nav-bar): added navbar options

--- apps/nextjs/src/components/Layout.tsx
+++ apps/nextjs/src/components/Layout.tsx
@@ -6,14 +6,15 @@ import { User, UserCircle2 } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from "~/components/ui/menubar";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "~/components/ui/navigation-menu";
 import icon from "../../public/logo.svg";
 import { Button } from "./ui/button";
+
+const ALLOWED_UNAUTHED_PATHS = ["/auth", "/"];
 
 function Layout(props: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
   const router = useRouter();
-
-  const ALLOWED_UNAUTHED_PATHS = ["/auth", "/"];
 
   if (status === "unauthenticated" && !ALLOWED_UNAUTHED_PATHS.includes(router.pathname)) router.push("/auth");
 
@@ -25,29 +26,61 @@ function Layout(props: { children: React.ReactNode }) {
         <Link href={"/"}>
           <Image src={icon} alt="SubM Logo" width={120} className="ml-4" />
         </Link>
+
+        <NavigationMenu className="absolute left-1/2 -translate-x-1/2 transform">
+          <NavigationMenuList>
+            {session?.user.role === "Admin" && (
+              <NavigationMenuItem>
+                <Link href="/vendor" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>Vendors</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            <NavigationMenuItem>
+              <Link href="/user" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Users</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/product" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Products</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            {session?.user.role === "Admin" && (
+              <NavigationMenuItem>
+                <Link href="/category" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>Categories</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+          </NavigationMenuList>
+        </NavigationMenu>
+
         {status === "unauthenticated" ? (
           <Button className="ml-auto mr-4 w-fit" onClick={() => router.push("/auth")}>
             Authenticate
           </Button>
         ) : (
-          <Menubar className="dark ml-auto mr-4 w-fit">
-            <MenubarMenu>
-              <MenubarTrigger className="m-0 p-2">
-                <User className="text-white" />
-              </MenubarTrigger>
-              <MenubarContent className="dark text-white">
-                <Profile />
-                <MenubarSeparator />
-                <Link href={`/profile/${session?.user.id}`}>
-                  <MenubarItem>Profile</MenubarItem>
-                </Link>
-                <Link href={`/settings`}>
-                  <MenubarItem>Settings</MenubarItem>
-                </Link>
-                <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
-              </MenubarContent>
-            </MenubarMenu>
-          </Menubar>
+          status === "authenticated" && (
+            <Menubar className="dark ml-auto mr-4 w-fit">
+              <MenubarMenu>
+                <MenubarTrigger className="m-0 p-2">
+                  <User className="text-white" />
+                </MenubarTrigger>
+                <MenubarContent className="dark text-white">
+                  <Profile />
+                  <MenubarSeparator />
+                  <Link href={`/profile/${session?.user.id}`}>
+                    <MenubarItem>Profile</MenubarItem>
+                  </Link>
+                  <Link href={`/settings`}>
+                    <MenubarItem>Settings</MenubarItem>
+                  </Link>
+                  <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
+          )
         )}
       </div>
       <div className={`flex flex-grow flex-col items-center justify-center text-white ${router.pathname !== "/auth" && "my-10"}`}>{props.children}</div>
