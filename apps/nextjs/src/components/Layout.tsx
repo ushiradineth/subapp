d1cf43f41e67cd173bf8f8e74c196ff7f17f12ff
Ushira Dineth <ushiradineth@gmail.com>
2023-05-21 17:05:26 +0530

    fix: improved nav components

--- apps/nextjs/src/components/Layout.tsx
+++ apps/nextjs/src/components/Layout.tsx
@@ -21,12 +21,39 @@ function Layout(props: { children: React.ReactNode }) {
   if (status === "authenticated" && router.pathname === "/auth") router.push("/");
 
   return (
-    <main className="bg-bgc dark flex flex-col min-h-screen">
+    <main className="bg-bgc dark flex min-h-screen flex-col">
       <div className={`border-bc flex h-14 items-center border-b ${router.pathname === "/auth" && "hidden"}`}>
         <Link href={"/"}>
           <Image src={icon} alt="SubM Logo" width={120} className="ml-4" />
         </Link>
+        <NavItems />
+        <AuthButton />
+      </div>
+      <div className={`flex flex-grow flex-col items-center justify-center text-white ${router.pathname !== "/auth" && "my-10"}`}>{props.children}</div>
+    </main>
+  );
+}
 
+export default Layout;
+
+function Profile() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      {session?.user.image ? <Image src={session?.user.image} alt={`${session?.user.name}'s profile image`} width={100} height={100} /> : <UserCircle2 width={100} height={100} />}
+      <p>{session?.user.name}</p>
+      <p>{session?.user.email}</p>
+    </div>
+  );
+}
+
+function NavItems() {
+  const { data: session, status } = useSession();
+
+  return (
+    <>
+      {status === "authenticated" && (
         <NavigationMenu className="absolute left-1/2 -translate-x-1/2 transform">
           <NavigationMenuList>
             {session?.user.role === "Admin" && (
@@ -55,49 +82,43 @@ function Layout(props: { children: React.ReactNode }) {
             )}
           </NavigationMenuList>
         </NavigationMenu>
-
-        {status === "unauthenticated" ? (
-          <Button className="ml-auto mr-4 w-fit" onClick={() => router.push("/auth")}>
-            Authenticate
-          </Button>
-        ) : (
-          status === "authenticated" && (
-            <Menubar className="dark ml-auto mr-4 w-fit">
-              <MenubarMenu>
-                <MenubarTrigger className="m-0 p-2">
-                  <User className="text-white" />
-                </MenubarTrigger>
-                <MenubarContent className="dark text-white">
-                  <Profile />
-                  <MenubarSeparator />
-                  <Link href={`/profile/${session?.user.id}`}>
-                    <MenubarItem>Profile</MenubarItem>
-                  </Link>
-                  <Link href={`/settings`}>
-                    <MenubarItem>Settings</MenubarItem>
-                  </Link>
-                  <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
-                </MenubarContent>
-              </MenubarMenu>
-            </Menubar>
-          )
-        )}
-      </div>
-      <div className={`flex flex-grow flex-col items-center justify-center text-white ${router.pathname !== "/auth" && "my-10"}`}>{props.children}</div>
-    </main>
+      )}
+    </>
   );
 }
 
-export default Layout;
-
-function Profile() {
-  const { data: session } = useSession();
+function AuthButton() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center p-4">
-      {session?.user.image ? <Image src={session?.user.image} alt={`${session?.user.name}'s profile image`} width={100} height={100} /> : <UserCircle2 width={100} height={100} />}
-      <p>{session?.user.name}</p>
-      <p>{session?.user.email}</p>
-    </div>
+    <>
+      {status === "unauthenticated" ? (
+        <Button className="ml-auto mr-4 w-fit" onClick={() => router.push("/auth")}>
+          Join Us
+        </Button>
+      ) : (
+        status === "authenticated" && (
+          <Menubar className="dark ml-auto mr-4 w-fit">
+            <MenubarMenu>
+              <MenubarTrigger className="m-0 p-2">
+                <User className="text-white" />
+              </MenubarTrigger>
+              <MenubarContent className="dark text-white">
+                <Profile />
+                <MenubarSeparator />
+                <Link href={`/profile/${session?.user.id}`}>
+                  <MenubarItem>Profile</MenubarItem>
+                </Link>
+                <Link href={`/settings`}>
+                  <MenubarItem>Settings</MenubarItem>
+                </Link>
+                <MenubarItem onClick={() => signOut()}>Log out</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        )
+      )}
+    </>
   );
 }
