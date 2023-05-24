edd47c400e66c6529d95b323c4becadf7c8a8794
Ushira Dineth <ushiradineth@gmail.com>
2023-05-25 00:00:48 +0530

    fix: added params and links

--- apps/nextjs/src/components/PageNumbers.tsx
+++ apps/nextjs/src/components/PageNumbers.tsx
@@ -1,22 +1,32 @@
 import React from "react";
-import { useRouter } from "next/router";
+import Link from "next/link";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
-function PageNumbers(props: { pageNumber: number; count: number; itemsPerPage: number; route: string }) {
-  const router = useRouter();
-
+function PageNumbers(props: { pageNumber: number; count: number; itemsPerPage: number; path: string; params: { [key: string]: unknown } }) {
   return (
     <div className="m-8 flex w-40 select-none justify-center">
       <div className="flex">
-        {props.pageNumber !== 1 ? <ChevronLeft onClick={() => router.push(`${props.route}?page=${props.pageNumber - 1}`)} /> : <p className="w-6"></p>}
+        {props.pageNumber !== 1 ? (
+          <Link href={{ href: props.path, query: { ...props.params, page: props.pageNumber - 1 } }}>
+            <ChevronLeft />
+          </Link>
+        ) : (
+          <p className="w-6"></p>
+        )}
 
         {[...Array(Math.ceil(props.count / props.itemsPerPage))].map((e, i) => (
-          <button className="mx-2" key={i} onClick={() => router.push(`${props.route}?page=${i + 1}`)}>
+          <Link href={{ href: props.path, query: { ...props.params, page: i + 1 } }} className="mx-2" key={i}>
             {i + 1}
-          </button>
+          </Link>
         ))}
 
-        {props.pageNumber !== Math.ceil(props.count / props.itemsPerPage) ? <ChevronRight onClick={() => router.push(`${props.route}?page=${props.pageNumber + 1}`)} /> : <p className="w-6"></p>}
+        {props.pageNumber !== Math.ceil(props.count / props.itemsPerPage) ? (
+          <Link href={{ href: props.path, query: { ...props.params, page: props.pageNumber + 1 } }}>
+            <ChevronRight />
+          </Link>
+        ) : (
+          <p className="w-6"></p>
+        )}
       </div>
     </div>
   );
