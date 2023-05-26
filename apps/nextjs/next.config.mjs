fee391dc85893a9d8a75967bd9889c4da99bb270
Ushira Dineth <ushiradineth@gmail.com>
2023-05-27 02:09:46 +0530

    chore: added supabase hostname

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -10,6 +10,14 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mslmfblxqxmjskebebfb.supabase.co",
+      }
+    ]
+  }
 };
 
 export default config;
