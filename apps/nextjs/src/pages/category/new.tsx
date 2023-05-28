62d7f5ebd069f9529a5307b701d033ffb1063d69
Ushira Dineth <ushiradineth@gmail.com>
2023-05-28 18:52:49 +0530

    chore: removed unused deps

--- apps/nextjs/src/pages/category/new.tsx
+++ apps/nextjs/src/pages/category/new.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Head from "next/head";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
