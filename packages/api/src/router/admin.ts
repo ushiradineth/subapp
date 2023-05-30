9438f2af9edd8dc6c9597096dc4630917654ee3e
Ushira Dineth <ushiradineth@gmail.com>
2023-05-30 22:23:13 +0530

    feat(api): added admin auth guard

--- packages/api/src/router/admin.ts
+++ packages/api/src/router/admin.ts
@@ -1,17 +1,10 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { adminProcedure, createTRPCRouter } from "../trpc";
 
 export const adminRouter = createTRPCRouter({
-  update: protectedProcedure.input(z.object({ id: z.string(), name: z.string() })).mutation(async ({ ctx, input }) => {
-    if (ctx.session.user.role !== "Admin") {
-      return new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Unauthorized Request",
-      });
-    }
-
+  update: adminProcedure.input(z.object({ id: z.string(), name: z.string() })).mutation(async ({ ctx, input }) => {
     const admin = await ctx.prisma.admin.update({ where: { id: input.id }, data: { name: input.name } });
 
     if (!admin) {
--- packages/api/src/router/category.ts
+++ packages/api/src/router/category.ts
@@ -1,10 +1,20 @@
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { adminProcedure, createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const categoryRouter = createTRPCRouter({
   create: protectedProcedure.input(z.object({ name: z.string(), description: z.string() })).mutation(async ({ ctx, input }) => {
     return await ctx.prisma.category.create({
+      data: {
+        name: input.name,
+        description: input.description,
+      },
+    });
+  }),
+
+  update: adminProcedure.input(z.object({ id: z.string(), name: z.string(), description: z.string() })).mutation(async ({ ctx, input }) => {
+    return await ctx.prisma.category.update({
+      where: { id: input.id },
       data: {
         name: input.name,
         description: input.description,
