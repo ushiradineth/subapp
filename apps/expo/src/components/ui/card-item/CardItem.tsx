3651b26890dca361856844d674275227e623f996
Ushira Dineth <ushiradineth@gmail.com>
2023-06-27 10:17:40 +0530

    feat(components): unified components

---
+++ apps/expo/src/components/ui/card-item/CardItem.tsx
@@ -0,0 +1,23 @@
+import React, { type ReactNode } from "react";
+import { Image, Text, YStack } from "tamagui";
+
+interface Props {
+  image: string;
+  onPress: () => void;
+  title: ReactNode;
+  text1?: ReactNode;
+}
+
+const CardItem = ({ image, title, text1, onPress }: Props) => {
+  return (
+    <YStack onPress={onPress} className="w-36">
+      <Image className="bg-foreground rounded-t-2xl" source={{ uri: image, height: 144, width: 144 }} alt={title?.toString()} />
+      <YStack className="bg-background flex h-12 w-full justify-center rounded-b-2xl pl-2">
+        <Text className="h-4 text-xs font-semibold">{title}</Text>
+        {text1 && <Text className="h-4 text-xs">{text1}</Text>}
+      </YStack>
+    </YStack>
+  );
+};
+
+export default CardItem;
