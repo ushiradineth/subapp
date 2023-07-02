c198ac404dc68560848a03a582975454278cf1ea
Ushira Dineth <ushiradineth@gmail.com>
2023-07-03 03:44:44 +0530

    feat(landing): fixed templates

---
+++ apps/nextjs/src/components/Templates/AdminDashboard.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function AdminDashboard() {
+  return (
+    <>
+      <Head>
+        <title>Admin Dashboard - SubM</title>
+      </Head>
+      <div className="flex flex-1 items-center justify-center">
+        <div className="mx-auto max-w-2xl">Admin Dashboard TODO</div>
+      </div>
+    </>
+  );
+}
+
+export default AdminDashboard;
