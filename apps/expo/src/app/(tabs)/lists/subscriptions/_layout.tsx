18c2e21942f55f7cd5b8a44777c036fdd9cb95b8
Ushira Dineth <ushiradineth@gmail.com>
2023-07-12 02:29:07 +0530

    feat(subscription): added subscription id page

--- apps/expo/src/app/(tabs)/lists/subscriptions/_layout.tsx
+++ apps/expo/src/app/(tabs)/lists/subscriptions/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from "expo-router";
 
+import BackButton from "~/components/Atoms/BackButton";
+
 const StackLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerTitle: "Subscriptions", headerShown: false }} />
-      <Stack.Screen name="[subscriptionId]" options={{ headerTitle: "Subscription", headerTitleAlign: "center" }} />
+      <Stack.Screen
+        name="[subscriptionId]"
+        options={{ headerTitle: "Subscription", headerTitleAlign: "center", presentation: "modal", headerLeft: () => <BackButton /> }}
+      />
     </Stack>
   );
 };
