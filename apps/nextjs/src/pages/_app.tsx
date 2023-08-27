877458a49483090f50907637da9d2496657be2e4
Ushira Dineth <ushiradineth@gmail.com>
2023-08-27 22:21:54 +0530

    feat(analytics): added vercel analytics

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import "../styles/globals.css";
 import type { AppType } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 import { ToastContainer } from "react-toastify";
 
+import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import NextNProgress from "nextjs-progressbar";
-
 import { api } from "~/utils/api";
 import Layout from "~/components/Layout";
 
@@ -14,9 +14,32 @@ const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { s
   return (
     <SessionProvider session={session}>
       <Layout>
-        <NextNProgress color="#5D76CC" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} options={{ showSpinner: false }} />
+        <NextNProgress
+          color="#5D76CC"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+          options={{ showSpinner: false }}
+        />
         <Component {...pageProps} />
-        <ToastContainer position="bottom-right" toastClassName={() => "bg-bgc border border-bc border relative flex p-2 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"} bodyClassName={() => "text-sm font-white font-med block flex flex-row p-3"} autoClose={5000} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+        <Analytics />
+        <ToastContainer
+          position="bottom-right"
+          toastClassName={() =>
+            "bg-bgc border border-bc border relative flex p-2 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
+          }
+          bodyClassName={() => "text-sm font-white font-med block flex flex-row p-3"}
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </Layout>
     </SessionProvider>
   );
